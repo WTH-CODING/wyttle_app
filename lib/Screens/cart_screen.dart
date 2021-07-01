63f6ae0d4f23c1bac8d58456d0f2c53b087eb593
shasank27 <shasankperiwal2027@gmail.com>
2021-07-02 00:00:56 +0530

    ProductCart Tile

--- lib/Screens/cart_screen.dart
+++ lib/Screens/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wyttle_app/data/color.dart';
 
 class CartScreen extends StatefulWidget {
   static String id = "CartScreen";
@@ -10,6 +11,142 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+        title: Text(
+          'My Cart',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: CartProductTile(),
+    );
+  }
+}
+
+class CartProductTile extends StatefulWidget {
+  @override
+  _CartProductTileState createState() => _CartProductTileState();
+}
+
+class _CartProductTileState extends State<CartProductTile> {
+  var quantity = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 140,
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Color(0xffC1C8E4),
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+      ),
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 10),
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('random_images/2.jpg'),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding:
+                  EdgeInsets.only(top: 20, left: 10, bottom: 15, right: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Product Name',
+                    style: TextStyle(fontWeight: FontWeight.w800, fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: 9,
+                  ),
+                  Text(
+                    'Size : 38',
+                    style: TextStyle(fontSize: 13, color: Colors.black54),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "₹159",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w800, fontSize: 18),
+                      ),
+                      Container(
+                        height: 35,
+                        width: 104,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  quantity = quantity - 1;
+                                });
+                              },
+                              icon: Icon(
+                                Icons.remove,
+                                color: Color(0xff5680E9),
+                              ),
+                            ),
+                            Text('$quantity'),
+                            IconButton(
+                              padding: EdgeInsets.all(0),
+                              onPressed: () {
+                                setState(() {
+                                  quantity = quantity + 1;
+                                });
+                              },
+                              icon: Icon(
+                                Icons.add,
+                                color: Color(0xff5680E9),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
