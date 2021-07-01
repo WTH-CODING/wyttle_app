37fd6a2c556246c45c5af2580812729025d34599
shasank27 <shasankperiwal2027@gmail.com>
2021-07-01 20:33:15 +0530

    Welcome Screen layout

--- lib/Screens/welcome_screen.dart
+++ lib/Screens/welcome_screen.dart
@@ -8,6 +8,7 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  String name = "Shasank";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +17,110 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         backgroundColor: Colors.white,
         leading: IconButton(
           onPressed: () {},
-          icon: IconAppBar(Icons.menu),
+          icon: IconAppBar(Icons.menu, 0),
         ),
-        actions: [IconButton(onPressed: () {}, icon: IconAppBar(Icons.search))],
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: IconAppBar(Icons.search, 0),
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Hello $name!',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              'Lets get something?',
+              textAlign: TextAlign.start,
+              style: TextStyle(color: Colors.black54),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 7),
+              height: 160,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  ScrollingImage('random_images/3.jpg', 270),
+                  ScrollingImage('random_images/2.jpg', 270),
+                  ScrollingImage('random_images/1.jpg', 270),
+                  ScrollingImage('random_images/0.jpg', 270),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Top Categories',
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  'SEE ALL',
+                  style: TextStyle(
+                      fontSize: 13, color: Theme.of(context).primaryColor),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 7),
+              height: 50,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  IconAppBar(Icons.watch, 10),
+                  IconAppBar(Icons.watch, 10),
+                  IconAppBar(Icons.watch, 10),
+                  IconAppBar(Icons.watch, 10),
+                  IconAppBar(Icons.watch, 10),
+                  IconAppBar(Icons.watch, 10),
+                  IconAppBar(Icons.watch, 10),
+                  IconAppBar(Icons.watch, 10),
+                  IconAppBar(Icons.watch, 10),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 7),
+              height: 190,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  ScrollingImage('random_images/3.jpg', 160),
+                  ScrollingImage('random_images/2.jpg', 160),
+                  ScrollingImage('random_images/1.jpg', 160),
+                  ScrollingImage('random_images/0.jpg', 160),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: Container(
         padding: EdgeInsets.only(bottom: 5),
@@ -33,27 +135,50 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               ),
             ),
             IconButton(
-              onPressed: () {},
-              icon: Icon(
-              Icons.bookmark_add,
-              color: Colors.black45,
-              )
-            ),
+                onPressed: () {},
+                icon: Icon(
+                  Icons.bookmark_add,
+                  color: Colors.black45,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.shopping_cart_rounded,
+                  color: Colors.black45,
+                )),
             IconButton(
               onPressed: () {},
               icon: Icon(
-              Icons.shopping_cart_rounded,
-              color: Colors.black45,
-              )
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-              Icons.person,
-              color: Colors.black45,
+                Icons.person,
+                color: Colors.black45,
               ),
-            ), 
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ScrollingImage extends StatelessWidget {
+  final String address;
+  final double wid;
+  ScrollingImage(this.address, this.wid);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: 160,
+        width: wid,
+        margin: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(25)),
+          image: DecorationImage(
+            image: AssetImage(address),
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
@@ -62,10 +187,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 
 class IconAppBar extends StatelessWidget {
   final IconData iconData;
-  IconAppBar(this.iconData);
+  final double marg;
+  IconAppBar(this.iconData, this.marg);
   @override
   Widget build(BuildContext context) {
     return Container(
+        width: 45,
+        margin: EdgeInsets.only(right: marg),
         padding: EdgeInsets.all(5),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(12)),
