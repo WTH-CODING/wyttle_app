5548b297dc1dc25a103e01fce829ce8a0dad9882
shasank27 <shasankperiwal2027@gmail.com>
2021-07-03 21:51:05 +0530

    Changed from GridView to GridView.builder

--- lib/Screens/product_screen.dart
+++ lib/Screens/product_screen.dart
@@ -8,6 +8,15 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
+  List<String> address = [
+    "random_images/3.jpg",
+    "random_images/2.jpg",
+    "random_images/1.jpg",
+    "random_images/0.jpg",
+    "random_images/3.jpg",
+    "random_images/2.jpg",
+    "random_images/1.jpg",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,40 +34,36 @@ class _ProductScreenState extends State<ProductScreen> {
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: GridView(
-        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: MediaQuery.of(context).size.width /
-              (MediaQuery.of(context).size.height / 1.5),
-        ),
-        children: [
-          LowerProducts('random_images/3.jpg'),
-          LowerProducts('random_images/2.jpg'),
-          LowerProducts('random_images/1.jpg'),
-          LowerProducts('random_images/0.jpg'),
-          LowerProducts('random_images/3.jpg'),
-        ],
+      body: Container(
+        color: Colors.blueGrey[50],
+        child: GridView.builder(
+            gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisSpacing: 1,
+              mainAxisSpacing: 2,
+              crossAxisCount: 2,
+              childAspectRatio: MediaQuery.of(context).size.width /
+                  (MediaQuery.of(context).size.height / 1.5),
+            ),
+            itemCount: address.length,
+            itemBuilder: (context, index) {
+              return ProductView(address[index]);
+            }),
       ),
     );
   }
 }
 
-class LowerProducts extends StatelessWidget {
+class ProductView extends StatelessWidget {
   final String address;
-  LowerProducts(this.address);
+  ProductView(this.address);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {},
       child: Container(
-        // height: 300,
-        // width: MediaQuery.of(context).size.width * 0.49,
-        // padding: EdgeInsets.all(1),
         margin: EdgeInsets.all(1),
         decoration: BoxDecoration(
-          // border: Border.all(color: Colors.black45, width: .8),
-          // borderRadius: BorderRadius.all(Radius.circular(12)),
           image: DecorationImage(
             image: AssetImage(address),
             fit: BoxFit.cover,
@@ -71,9 +76,6 @@ class LowerProducts extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  // SizedBox(
-                  //   width: 1,
-                  // ),
                   Container(
                     padding:
                         EdgeInsets.only(left: 7, right: 7, top: 2, bottom: 2),
@@ -85,10 +87,10 @@ class LowerProducts extends StatelessWidget {
                       ),
                     ),
                     decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(12),
+                          bottomRight: Radius.circular(12)),
                       color: Colors.white,
-                      // borderRadius: BorderRadius.all(
-                      //   Radius.circular(12),
-                      // ),
                     ),
                   ),
                   IconButton(
@@ -106,10 +108,6 @@ class LowerProducts extends StatelessWidget {
                 height: 50,
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  // borderRadius: BorderRadius.only(
-                  //   bottomLeft: Radius.circular(12),
-                  //   bottomRight: Radius.circular(12),
-                  // )
                 ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
